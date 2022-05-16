d3a11ceddab25ceae3329a9e3f049b5fe02303fc
mfrancisco <francisco.melanie@gmail.com>
2022-05-16 14:47:55 -0700

    add dog model, db, detail page and url, and superuser

--- dogcollector/main_app/models.py
+++ dogcollector/main_app/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100) 
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
 
-dogs_list = [
-    Dog('Cookie', 'Labrador Retriever', 'brown', 4),
-    Dog('Frenchie', 'French Bulldog', 'black and white', 2),
-    Dog('Orion', 'Siberian Husky', 'white', 0)
-]
+    def __str__(self):
+        return self.name
--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import *
+from .models import Dog
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 
 # Create your views here.
 def home(request):
@@ -18,4 +22,19 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
-    return render(request, 'dogs/index.html', {'dogs': dogs_list})
+    """
+    dogs index page
+    http://localhost:8000/dogs/
+    """
+    logging.info('calling dogs_index')
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_detail(request, dog_id):
+    """
+    dog detail page
+    http://localhost:8000/dogs/1/
+    """
+    logging.info('calling dog_detail')
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {'dog': dog})
