c1dfff22796242de77c58be1a23f47e51da1c0a0
mfrancisco <francisco.melanie@gmail.com>
2022-05-17 16:54:51 -0700

    add CUD

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 from .models import Dog
 
 import logging
@@ -12,7 +13,7 @@ def home(request):
     home view
     http://localhost:8000/
     """
-    return HttpResponse('<h1>Hello</h1>')
+    return render(request, 'home')
 
 def about(request):
     """
@@ -38,3 +39,28 @@ def dogs_detail(request, dog_id):
     logging.info('calling dog_detail')
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+class DogCreate(CreateView):
+    """
+    This class will create a dog object
+    """
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+    """
+    This class will update a dog object from the DB
+    """
+    model = Dog
+    fields = ['description', 'age']
+    
+    def get_success_url(self, **kwargs):
+        return reverse('detail', args=(self.object.id,))
+
+class DogDelete(DeleteView):
+    """
+    This class will delete a dog object from the DB
+    """
+    model = Dog
+    success_url = '/dogs/'
