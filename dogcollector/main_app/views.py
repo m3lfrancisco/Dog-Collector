26f339278c108867ead8e51cbcad00b73dcf0296
mfrancisco <francisco.melanie@gmail.com>
2022-05-22 10:44:00 -0700

    add django authentication and authorization

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -4,6 +4,10 @@ import uuid
 from django.shortcuts import render, redirect 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from .models import Dog, Toy, Photo
 from .forms import FeedingForm
@@ -11,7 +15,6 @@ from .forms import FeedingForm
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Create your views here.
 def home(request):
     """
     home view
@@ -26,6 +29,7 @@ def about(request):
     """
     return render(request, 'about.html')
 
+@login_required
 def dogs_index(request):
     """
     dogs index page
@@ -35,6 +39,7 @@ def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
+@login_required
 def dogs_detail(request, dog_id):
     """
     dog detail page
@@ -49,6 +54,45 @@ def dogs_detail(request, dog_id):
         'toys': toys_dog_doesnt_have
         })
 
+class DogCreate(LoginRequiredMixin, CreateView):
+    """
+    This class will create a dog object
+    """
+    model = Dog
+    fields = ['name', 'breed', 'description', 'age']
+    success_url = '/dogs/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class DogUpdate(LoginRequiredMixin, UpdateView):
+    """
+    This class will update a dog object from the DB
+    """
+    model = Dog
+    fields = ['description', 'age']
+    
+    def get_success_url(self, **kwargs):
+        return reverse('detail', args=(self.object.id,))
+
+class DogDelete(LoginRequiredMixin, DeleteView):
+    """
+    This class will delete a dog object from the DB
+    """
+    model = Dog
+    success_url = '/dogs/'
+
+@login_required
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
+
+@login_required
 def add_photo(request, dog_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
@@ -63,61 +107,44 @@ def add_photo(request, dog_id):
             print('An error occurred while uploading file to S3')
     return redirect('detail', dog_id=dog_id)
 
-class DogCreate(CreateView):
-    """
-    This class will create a dog object
-    """
-    model = Dog
-    fields = ['name', 'breed', 'description', 'age']
-    success_url = '/dogs/'
-
-class DogUpdate(UpdateView):
-    """
-    This class will update a dog object from the DB
-    """
-    model = Dog
-    fields = ['description', 'age']
-    
-    def get_success_url(self, **kwargs):
-        return reverse('detail', args=(self.object.id,))
-
-class DogDelete(DeleteView):
-    """
-    This class will delete a dog object from the DB
-    """
-    model = Dog
-    success_url = '/dogs/'
-
-def add_feeding(request, dog_id):
-    form = FeedingForm(request.POST)
-    if form.is_valid():
-        new_feeding = form.save(commit=False)
-        new_feeding.dog_id = dog_id
-        new_feeding.save()
-    return redirect('detail', dog_id=dog_id)
-
+@login_required
 def assoc_toy(request, dog_id, toy_id):
     Dog.objects.get(id=dog_id).toys.add(toy_id)
     return redirect('detail', dog_id=dog_id)
 
+@login_required
 def unassoc_toy(request, dog_id, toy_id):
     Dog.objects.get(id=dog_id).toys.remove(toy_id)
     return redirect('detail', dog_id=dog_id)
 
-class ToyList(ListView):
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up, please try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
